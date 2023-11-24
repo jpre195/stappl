4f45a0a43c50ecc23c3ba54e359a9c2b97e0b6dd
jpre195 <jdpreston7@gmail.com>
2023-11-24 16:11:27 -0500

    Added code for paired t-test

--- Home.py
+++ Home.py
@@ -6,3 +6,14 @@ import numpy as np
 st.title('Statistical Testing APPLication')
 
 st.write('This is the homepage')
+
+#Upload a file
+file = st.file_uploader('Choose a file', type = ['csv'])
+
+if file is not None:
+
+    df = pd.read_csv(file)
+
+    st.session_state['df'] = df
+
+    st.dataframe(df.head(10), use_container_width = True, hide_index = True)
